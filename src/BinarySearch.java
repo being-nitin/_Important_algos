f4c1bdd2d99bc7bb7fc218f7d8d9723051e7f1c8
Nitin Singh <nsinghboss02@gmail.com>
2022-11-26 10:38:51 +0530

    Binary search algorithm.

---
+++ src/BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    // Binary search algorithm
+    // Worst case time complexity O(logn)
+    public static int searchB(int[] arr,int target,int n){
+        int start = 0;
+        int end = n;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(arr[mid]==target)
+                return mid;
+            else if(arr[mid]>target)
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+        return -1;
+    }
+}
