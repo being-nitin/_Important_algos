7fa2a1182b38b060cbc6b2a346a9f9112aad5c56
Nitin Singh <nsinghboss02@gmail.com>
2022-11-26 10:33:14 +0530

    Linear search algorithm.

---
+++ src/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+    // Linear search algorithm
+    public static int search(int[] arr,int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
