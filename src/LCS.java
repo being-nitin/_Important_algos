1fceb833b1cf1f5e467a0c1358d38530f775e51d
Nitin Singh <nsinghboss02@gmail.com>
2022-12-15 13:41:35 +0530

    Lowest common subsequence

---
+++ src/LCS.java
@@ -0,0 +1,23 @@
+public class LCS{
+    public int longestCommonSubsequence(String text1, String text2) {
+        // will convert the given string into character array to make it more compatible for             traversal
+        char[] ch1 = text1.toCharArray();
+        char[] ch2 = text2.toCharArray();
+        int[][] dp = new int[ch1.length+1][ch2.length+1];
+        int max=0;
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[i].length;j++){
+                if(ch1[i-1]==ch2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+                if(dp[i][j]>max){
+                    max = dp[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}
